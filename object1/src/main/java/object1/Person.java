b6be24ae1a988d9c3f9c1aa89bfb21c8c93c3cfa
Josue <josue@zipcodersmbp10.lan>
2024-06-25 20:06:00 -0400

    newcommit

---
+++ object1/src/main/java/object1/Person.java
@@ -0,0 +1,22 @@
+// Define a class
+class Person {
+    // Define Instance Variables
+    // different for each object!
+    String name;
+    int age = 0;
+
+    // Constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        // Declare and initialize an object
+        Person person1 = new Person(John, 30);
+
+    /* Now you can use the Object  */
+        
+    }
+}
+
